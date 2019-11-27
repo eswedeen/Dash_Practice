4ccc938f8910ea373595f5e14cd9136aec9d63fa
Erik <erikswedeen@gmail.com>
2019-11-26 22:44:14 -0600

    added examples

--- app.py
+++ app.py
@@ -1,105 +1,170 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 import pandas as pd
 
+app = dash.Dash(__name__)
 
-
-def generate_table(dataframe, max_rows=5):
-    return html.Table(
-        #Header
-        [html.Tr([html.Th(col) for col in dataframe.columns])] +
-
-        #Body
-        [html.Tr([
-            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-        ]) for i in range(min(len(dataframe), max_rows))]
-    )
-
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF',
-    'dfbackground': '#808FDF'
-}
-
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style = {
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-
-
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [8, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-                {'x': [1, 2, 3], 'y': [7, 9, 5], 'type': 'bar', 'name': 'AZ'}
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization',
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
+bar1 = dcc.Graph(
+    id='bar1',
+    figure={
+        'data': [
+            {'x': [1, 2, 3], 'y': [8, 1, 2], 'type': 'bar', 'name': 'SF'},
+            {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            {'x': [1, 2, 3], 'y': [7, 9, 5], 'type': 'bar', 'name': 'AZ'}
+        ],
+        'layout': {
+            'title': 'Dash Data Visualization 1',
+            'plot_bgcolor': '#0F1536',
+            'paper_bgcolor': '#0F1536',
+            'height':'300',
+            'font': {
+                'color': 'white'
             }
         }
-    ),
+    }
+)
 
-    html.H2(
-        children = 'UFC DataFrame',
-        style = {
-            'textAlign': 'center',
-            'color': colors['text']
-        } 
-    ),
-
-    html.Div(
-        children = [
-            html.H4(
-                children = 'UFC Fighters Statistics (1993-2019)',
-                style = {
-                    'textAlign': 'left',
-                    'color': 'black'
-                }
-            ),
-
-            html.P(
-                children ='''This is a list of UFC fighter statistics for the total history of the organization. the following data represents results from fighters
-                    bouts including information such as: Fighter Name, Referee, date of event, location, weight class, etc.''',
-                style = {
-                    'textAlign': 'left',
-                    'color': 'black'
-                }
-            ),   
-
-            generate_table(df)
+bar2 = dcc.Graph(
+    id='bar2',
+    figure={
+        'data': [
+            {'x': [1, 2, 3], 'y': [8, 1, 2], 'type': 'bar', 'name': 'SF'},
+            {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            {'x': [1, 2, 3], 'y': [7, 9, 5], 'type': 'bar', 'name': 'AZ'}
         ],
-        style = {
-            'backgroundColor': 'white'
+        'layout': {
+            'title': 'Dash Data Visualization 2',
+            'plot_bgcolor': '#0F1536',
+            'paper_bgcolor': '#0F1536',
+            'height':'300',
+            'font': {
+                'color': 'white'
+            }
         }
-    ),
+    }
+)
+
+bar3 = dcc.Graph(
+    id='bar3',
+    figure={
+        'data': [
+            {'x': [1, 2, 3], 'y': [8, 1, 2], 'type': 'bar', 'name': 'SF'},
+            {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            {'x': [1, 2, 3], 'y': [7, 9, 5], 'type': 'bar', 'name': 'AZ'}
+        ],
+        'layout': {
+            'title': 'Dash Data Visualization 3',
+            'plot_bgcolor': '#0F1536',
+            'paper_bgcolor': '#0F1536',
+            'height':'300',
+            'font': {
+                'color': 'white'
+            }
+        },
+    }
+)
+
+bar4 = dcc.Graph(id='bar4')
+
+map = html.Iframe(
+    id = 'map',
+    srcDoc = open('map.html', 'r').read(),
+    width = '100%',
+    height = '100%'
+)
+
+slider = dcc.Slider(
+    id='slider-input',
+    min=0,
+    max=9,
+    marks={i: 'Date {}'.format(i) for i in range(10)},
+    value=4,
+)
+
+textinput = dcc.Input(id='textin-id', value='initial value', type='text')
+textout = html.Div(id='textout-div')
 
 
 
+mainContainer = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        bar1,
+                        bar2,
+                        #bar3
+                        bar4
+                    ],
+                    width = 4,
+                ),
 
+                dbc.Col(
+                    [
+                        map
+                    ],
+                    width = 8
+                )
+            ],
+        ),
 
-])
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        textinput,
+                        textout
+                    ],
+                    width = 4
+                ),
+
+                dbc.Col(
+                    [
+                        slider,
+                    ],
+                    width = 4
+                )
+            ]
+        )   
+    ],
+    fluid=True
+    #style={'backgroundColor':'#212748'} #373C5A
+)
+
+app.layout = html.Div(
+    [
+        mainContainer        
+    ],
+)
+
+@app.callback(
+    Output('bar4', 'figure'),
+    [Input('slider-input', 'value')])
+def update_figure(selected_year):
+    
+    traces = [
+        {'x': [1], 'y': [selected_year], 'type': 'bar', 'name': 'Slider Out'},
+        {'x': [2], 'y': [selected_year], 'type': 'bar', 'name': 'Slider Out'},
+    ]
+
+    return {
+        'data': traces,
+        'layout': {
+            'title': 'Dash Data Visualization 4',
+            'yaxis': {'title': 'OUT Y', 'range': [0, 10]},
+            'plot_bgcolor': '#0F1536',
+            'paper_bgcolor': '#0F1536',
+            'height':'300',
+            'font': {
+                'color': 'white'
+            }
+        },
+    }
+    
 
 if __name__ == '__main__':
     app.run_server(debug=True)
